72516f9d56f9baf3d1e06e926a6c5570233f77e1
Kunal Jadhav <kunaljadhav2305@gmail.com>
2024-08-19 22:57:50 +0530

    DataType 2

---
+++ 01-Basic/02_DataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "Kunal"
+let age = 18
+let islogin = true
+
+// PRIMITIVE DataType
+// number
+// bigInt
+// String => ""
+// boolean => true false
+// null => standalone value || value in empty
+// undefined => varabile is declair but value is undefined
+// Symbol => uniqueness
+
+// Object
+
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
