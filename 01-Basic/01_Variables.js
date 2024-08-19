5f8d14201947f50f9b0054e4f16bbe7cc4c9ea3a
Kunal Jadhav <kunaljadhav2305@gmail.com>
2024-08-19 22:57:29 +0530

    Variable 1

---
+++ 01-Basic/01_Variables.js
@@ -0,0 +1,17 @@
+const accId = "1234"
+let accEmail = "Kunla@123.gmail.com"
+var accPass = "12398"
+accCity = "Mumbai"
+
+// accId = 2 not allowed to change value in const
+
+accEmail = "jadhav@123.com"    //This can be change in feture 
+accPass = "765"   //This can be change in feture 
+accCity = "Pune"   //This can be change in feture 
+
+console.log(accEmail);
+//we should not use var use insterd let
+
+console.table([accEmail, accPass, accCity,])
+
+ 
