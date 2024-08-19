13cbdb52f2b1e7976e6ff9290ab8c415c312b3ed
Kunal Jadhav <kunaljadhav2305@gmail.com>
2024-08-19 22:58:12 +0530

    Operations 3

---
+++ 01-Basic/03_Operations.js
@@ -0,0 +1,49 @@
+let value = 4
+let newValue = -value
+// console.log(newValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "kunal"
+let str2 = " Jadhav"
+
+// console.log(str1 + str2);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+
+let gameCounter = 100
+// gameCounter++
+console.log(gameCounter++); //Prefix Increment (++variable): Increments the variable first, then returns the value.
+console.log(++gameCounter); //Postfix Increment (variable++): Returns the current value of the variable, then increments it.
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
